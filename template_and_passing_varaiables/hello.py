f3a474925cc9b22aabb91277c9205df8c3085886
saurabh2k26 <saurabh2k26@gmail.com>
2018-07-20 15:40:54 +0530

    Create hello.py

---
+++ template_and_passing_varaiables/hello.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('hello.html', name = 'saurabh', marks = 60)
+
+if __name__ == '__main__':
+   app.run(debug = True)
